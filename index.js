e803bab1515d363f8c071b419143d8a8097d7256
Hyuugo <rudnov.d@gmail.com>
2018-08-06 19:28:27 +0500

    enable CORS

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.set('port', PORT);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug'); 
 
+
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
